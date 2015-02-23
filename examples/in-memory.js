3ab30caad5f937fc9ececc32fc9fe2fe9b79a892
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-02-23 14:57:20 +0100

    add in memory example

---
+++ examples/in-memory.js
@@ -0,0 +1,20 @@
+var ltest = require('../')({ mem: true })
+ltest('in memory', function (t, db, createReadStream) {
+  var batch = [
+    { type: 'put', key: 'key1', value: 'value1' },
+    { type: 'put', key: 'key2', value: 'value2' },
+    { type: 'put', key: 'key3', value: 'value3' }
+  ]
+  db.batch(batch, function (err) {
+    t.ok(!err, 'no batch error')
+    var count = 0
+    createReadStream()
+      .on('data', function (data) {
+        ++count
+      })
+      .on('end', function () {
+        t.equal(count, 3)
+        t.end()
+      })
+  })
+})
